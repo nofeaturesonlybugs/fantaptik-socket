49cf9a95252a9a0bae5727b0d35136f4d3bbe0fa
Nofeatures Onlybugs <nofeaturesonlybugs@gmail.com>
2020-09-08 13:19:33 -0700

    src/Plugins/Plugin.js: Update jsdoc for `Socket` property.

--- src/Plugins/Plugin.js
+++ src/Plugins/Plugin.js
@@ -4,10 +4,20 @@
  * A `Plugin` does not have to implement the entire interface.  When an interface method is implemented
  * the `Socket` instance will call the `Plugin` method at the appropriate time of its lifecycle and activity.  
  * 
+ * Plugins need to support a `Socket` property that will be assigned the instantiated `Socket` they are attached to.  
+ * 
  * @class
  * @interface
  */
 export class Plugin {
+    /**
+     * `Socket` is the `Socket` instance attached to the plugin; it is not available in the `Plugin`'s constructor but
+     * is available in the other methods.  
+     * 
+     * @property {Socket}
+     * @name Plugin#Socket
+     */
+
     /**
      * onconnect is called when the WebSocket fires the `onconnect` event.
      * 
